7b30403499f6d1b178de81a1b0c85a5dd93f9ef2
ocoa <shawn@ocoa.com>
2018-09-21 11:30:19 -0400

    fix var

--- hashicorp/solodev-phpfpm-72/scripts/configure-single-ami.sh
+++ hashicorp/solodev-phpfpm-72/scripts/configure-single-ami.sh
@@ -11,7 +11,7 @@ test -n "\$EC2_INSTANCE_ID" || die 'cannot obtain instance-id'
 EC2_AVAIL_ZONE="`wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone || die \"wget availability-zone has failed: $?\"`"
 test -n "\$EC2_AVAIL_ZONE" || die 'cannot obtain availability-zone'
 EC2_REGION="\`echo "\$EC2_AVAIL_ZONE" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'\`"
-MOUNT = "/var/www/Solodev/clients/solodev"
+MOUNT="/var/www/Solodev/clients/solodev"
 
 echo "Create Solodev database and user"
 echo "CREATE DATABASE solodev;" >> /tmp/setup.mysql
