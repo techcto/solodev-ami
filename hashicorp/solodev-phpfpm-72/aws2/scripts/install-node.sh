5097168e9942d49ea435a158f4afc66407ae391b
ocoa <shawn@ocoa.com>
2018-10-31 16:58:21 -0400

    update npm path to cire dir

--- hashicorp/solodev-phpfpm-72/aws2/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/aws2/scripts/install-node.sh
@@ -1,9 +1,9 @@
 #Init
-mkdir -p /var/www/solodev/clients/solodev/.node_modules_global
-export PATH=/var/www/solodev/clients/solodev/.node_modules_global/bin:$PATH
+mkdir -p /var/www/solodev/.node_modules_global
+export PATH=/var/www/solodev/.node_modules_global/bin:$PATH
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 
 #Execute
-npm config set prefix '/var/www/solodev/clients/solodev/.node_modules_global'
+npm config set prefix '/var/www/solodev/.node_modules_global'
 yum install -y --enablerepo=nodesource nodejs
 npm install -g autoprefixer clean-css-cli nodemon npm-run-all postcss-cli postcss-discard-empty shx uglify-js node-sass
--- hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
@@ -1,9 +1,9 @@
 #Init
-mkdir -p /var/www/Solodev/clients/solodev/.node_modules_global
-export PATH=/var/www/Solodev/clients/solodev/.node_modules_global/bin:$PATH
+mkdir -p /var/www/Solodev/.node_modules_global
+export PATH=/var/www/Solodev/.node_modules_global/bin:$PATH
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 
 #Execute
-npm config set prefix '/var/www/Solodev/clients/solodev/.node_modules_global'
+npm config set prefix '/var/www/Solodev/.node_modules_global'
 yum install -y --enablerepo=nodesource nodejs
 npm install -g autoprefixer clean-css-cli nodemon npm-run-all postcss-cli postcss-discard-empty shx uglify-js node-sass
