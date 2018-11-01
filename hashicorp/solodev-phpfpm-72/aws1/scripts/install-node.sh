474a6db312027ad23d6d2c1a0382bbfffea0cc17
ocoa <shawn@ocoa.com>
2018-11-01 14:28:41 -0400

    ..

--- hashicorp/solodev-phpfpm-72/aws1/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/aws1/scripts/install-node.sh
@@ -1,2 +1,3 @@
 #Init
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
+yum install -y nodejs
--- hashicorp/solodev-phpfpm-72/aws2/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/aws2/scripts/install-node.sh
@@ -1,9 +1,3 @@
 #Init
-mkdir -p /var/www/solodev/.node_modules_global
-export PATH=/var/www/solodev/.node_modules_global/bin:$PATH
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
-
-#Execute
-npm config set prefix '/var/www/solodev/.node_modules_global'
-yum install -y --enablerepo=nodesource nodejs
-npm install -g autoprefixer clean-css-cli nodemon npm-run-all postcss-cli postcss-discard-empty shx uglify-js node-sass
+yum install -y nodejs
