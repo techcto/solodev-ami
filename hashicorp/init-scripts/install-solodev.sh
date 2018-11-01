e9b89108d38900a0c99d32171b3d61338a40650d
ocoa <shawn@ocoa.com>
2018-10-31 23:10:27 -0400

    ..

--- hashicorp/init-scripts/install-solodev.sh
+++ hashicorp/init-scripts/install-solodev.sh
@@ -4,4 +4,5 @@ fn="$(aws s3 ls s3://solodev-release | sort | tail -n 1 | awk '{print $4}')"
 aws s3 cp s3://solodev-release/$fn /tmp/Solodev/Solodev.zip
 cd /tmp/Solodev/
 unzip Solodev.zip
+rm -Rf Solodev.zip
 ls -al
