f2d9b281c738e7c75dcb431b63e3d500697c29cf
ocoa <shawn@ocoa.com>
2018-11-01 23:29:40 -0400

    ..

--- hashicorp/solodev-phpfpm-72/aws1/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/aws1/scripts/install-node.sh
@@ -1,3 +1,5 @@
-#Init
-curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
-yum install -y --enablerepo=nodesource nodejs
+# Install Node.js 6.x repository
+curl -sL https://rpm.nodesource.com/setup_6.x | bash -
+
+# Install Node.js and npm
+yum install nodejs
