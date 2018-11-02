93bcc19bd6a95323a24cbeb384fef7f9bc2f7194
ocoa <shawn@ocoa.com>
2018-11-02 18:55:18 -0400

    ..

--- hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
@@ -2,10 +2,11 @@ curl -sL https://rpm.nodesource.com/setup_11.x | sudo -E bash -
 yum install -y --enablerepo=nodesource nodejs
 
 mkdir -p "/var/www/node_modules_global"
+mkdir /var/www/.npm
+
 npm config set prefix "/var/www/node_modules_global"
 npm install -g autoprefixer clean-css-cli npm-run-all postcss-cli postcss-discard-empty shx uglify-js 
 npm install -g --unsafe-perm node-sass
 
-mkdir /var/www/.npm
-chown -Rf apache.apache /var/www/.npm
-chmod -Rf 2770 /var/www/.npm
+chown -Rf apache.apache /var/www/.npm /var/www/node_modules_global
+chmod -Rf 2770 /var/www/.npm /var/www/node_modules_global
