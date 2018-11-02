0efe10ef174c304d68296382478783897b75432d
ocoa <shawn@ocoa.com>
2018-11-02 12:54:17 -0400

    ..

--- hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
@@ -5,3 +5,7 @@ mkdir -p "/var/www/node_modules_global"
 npm config set prefix "/var/www/node_modules_global"
 npm install -g autoprefixer clean-css-cli npm-run-all postcss-cli postcss-discard-empty shx uglify-js 
 npm install -g --unsafe-perm node-sass
+
+mkdir /var/www/.npm
+chown -Rf apache.apache /var/www/.npm
+chmod -Rf 2770 /var/www/.npm
