90335c2b1e3c49ff7bc15b1441b484523faebacc
ocoa <shawn@ocoa.com>
2018-11-01 21:14:58 -0400

    ..

--- hashicorp/solodev-phpfpm-72/aws1/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/aws1/scripts/install-node.sh
@@ -1,6 +1,3 @@
 #Init
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 yum install -y --enablerepo=nodesource nodejs
-npm config set prefix '/var/www/solodev/node_modules_global'
-echo "export NODE_PATH=/var/www/solodev/node_modules_global/lib/node_modules" >>  "/var/www/solodev/node_modules_global/.npmrc"
-echo "export PATH=$PATH:/var/www/solodev/node_modules_global/bin" >>  "/var/www/solodev/node_modules_global/.npmrc"
--- hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
@@ -1,6 +1,3 @@
 #Init
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 yum install -y --enablerepo=nodesource nodejs
-npm config set prefix '/var/www/Solodev/node_modules_global'
-echo "export NODE_PATH=/var/www/Solodev/node_modules_global/lib/node_modules" >>  "/var/www/Solodev/node_modules_global/.npmrc"
-echo "export PATH=$PATH:/var/www/Solodev/node_modules_global/bin" >>  "/var/www/Solodev/node_modules_global/.npmrc"
