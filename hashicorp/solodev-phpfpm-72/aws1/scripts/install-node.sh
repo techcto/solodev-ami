411db73810ee783bbd769f36d02eaffbed0002e2
ocoa <shawn@ocoa.com>
2019-02-22 15:14:53 -0500

    add nodemon to npm globals

--- hashicorp/solodev-phpfpm-72/aws1/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/aws1/scripts/install-node.sh
@@ -3,7 +3,7 @@ yum install -y --enablerepo=nodesource nodejs
 
 mkdir -p "/var/www/node_modules_global"
 npm config set prefix "/var/www/node_modules_global"
-npm install -g autoprefixer clean-css-cli npm-run-all postcss-cli postcss-discard-empty shx uglify-js 
+npm install -g autoprefixer clean-css-cli npm-run-all postcss-cli postcss-discard-empty shx nodemon uglify-js 
 npm install -g --unsafe-perm node-sass
 
 mkdir /var/www/.npm
