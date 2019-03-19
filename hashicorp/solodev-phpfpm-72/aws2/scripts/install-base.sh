3f720bd2214670a14babe08a0834e306f04d8912
greg@solodev.com <greg@solodev.com>
2019-03-19 09:13:14 -0400

    sendmail install on single and removed from aws2

--- hashicorp/solodev-phpfpm-72/aws2/scripts/install-base.sh
+++ hashicorp/solodev-phpfpm-72/aws2/scripts/install-base.sh
@@ -2,7 +2,7 @@
 yum -y remove php* httpd*
 
 #Install Required Devtools
-yum -y install gcc-c++ gcc pcre-devel make zip unzip wget curl cmake git yum-utils sudo sendmail
+yum -y install gcc-c++ gcc pcre-devel make zip unzip wget curl cmake git yum-utils sudo
 
 #Install Required Repos
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
