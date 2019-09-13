1ed3da0b0574e86a95dbba21b20158d28e2fd570
techcto <smoore@solodev.com>
2019-09-13 09:48:22 -0400

    ..

--- hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
@@ -1,5 +1,6 @@
 curl -sL https://rpm.nodesource.com/setup_11.x | sudo -E bash -
 yum install -y --enablerepo=nodesource nodejs
+
 npm install -g --unsafe-perm @fortawesome/fontawesome-free autoprefixer clean-css-cli node-sass npm-run-all postcss postcss-cli gulp gulp-autoprefixer \
 		gulp-clean gulp-clean-css gulp-concat gulp-rename gulp-sass gulp-uglify node-sass
 
