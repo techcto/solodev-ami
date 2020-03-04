f6dc110ecbf77ecc57c9fd38cb8c2753463332bb
techcto <smoore@solodev.com>
2020-03-04 15:05:11 -0500

    add java

--- hashicorp/solodev-phpfpm-72/single/scripts/install-base.sh
+++ hashicorp/solodev-phpfpm-72/single/scripts/install-base.sh
@@ -2,7 +2,7 @@
 yum -y remove php* httpd*
 
 #Install Required Devtools
-yum -y install gcc-c++ gcc pcre-devel make zip unzip wget curl cmake git yum-utils sudo
+yum -y install gcc-c++ gcc pcre-devel make zip unzip wget curl cmake git yum-utils sudo java
 
 #Install Required Repos
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
