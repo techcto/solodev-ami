9d7b985ca9065b5a1c68cff57001d62dcf3dd1a2
techcto <smoore@solodev.com>
2020-03-05 12:08:14 -0500

    ..

--- hashicorp/solodev-phpfpm-72/single/scripts/configure-solodev.sh
+++ hashicorp/solodev-phpfpm-72/single/scripts/configure-solodev.sh
@@ -72,5 +72,7 @@ echo "IncludeOptional /var/www/solodev/clients/solodev/s.Vhosts/*.*" >> /etc/htt
 mv /tmp/restart.php /root/restart.php
 mv /tmp/check.sh /root/check.sh
 mv /tmp/client.env /root/client.env
+sed -i "s/REPLACE_WITH_LICENSE_SERVER/$LICENSE_SERVER/g" /root/check.sh
+sed -i "s/REPLACE_WITH_DOCUMENT_ROOT/$DOCUMENT_ROOT/g" /root/check.sh
 (crontab -l 2>/dev/null; echo "*/2 * * * * php /root/restart.php") | crontab -
 (crontab -l 2>/dev/null; echo "0,15,30,45 * * * * php /root/check.sh") | crontab -
