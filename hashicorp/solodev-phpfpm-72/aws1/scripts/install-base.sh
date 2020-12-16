ad5ea96f407ec2889b60628cb8ce5c149ecf94ae
Ross Williams <ross@solodev.com>
2020-12-16 10:25:20 -0500

    updating expired packages for aws1

--- hashicorp/solodev-phpfpm-72/aws1/scripts/install-base.sh
+++ hashicorp/solodev-phpfpm-72/aws1/scripts/install-base.sh
@@ -3,13 +3,13 @@ yum -y remove php* httpd*
 
 #Install Required Devtools
 yum -y install gcc-c++ gcc pcre-devel make zip unzip wget curl cmake git yum-utils sudo sendmail
-wget http://195.220.108.108/linux/epel/6/x86_64/Packages/s/scl-utils-20120229-1.el6.x86_64.rpm
+wget https://download-ib01.fedoraproject.org/pub/epel/6/x86_64/Packages/s/scl-utils-20120229-1.el6.x86_64.rpm
 rpm -Uvh scl-utils-20120229-1.el6.x86_64.rpm
 
 #Install Required Repos
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+wget http://mirror.math.princeton.edu/pub/fedora-archive/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.remirepo.net/enterprise/remi-release-6.rpm
-rpm -Uvh epel-release-latest-6.noarch.rpm
+rpm -Uvh epel-release-6-8.noarch.rpm
 rpm -Uvh remi-release-6.rpm
 yum-config-manager --enable remi-php72
 yum --enablerepo=epel --disablerepo=amzn-main -y install libwebp
