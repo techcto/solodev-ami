25d5d91570829ade5b43d146ff533bc9422f9c7b
techcto <smoore@solodev.com>
2021-05-11 18:37:36 -0400

    ..

--- hashicorp/init-scripts/install-solodev.sh
+++ hashicorp/init-scripts/install-solodev.sh
@@ -1,5 +1,6 @@
 #Install latest official release of Solodev
 mkdir -p /tmp/Solodev
+aws --version
 fn="$(aws s3 ls s3://solodev-release | sort | tail -n 1 | awk '{print $4}')"
 aws s3 cp s3://solodev-release/$fn /tmp/Solodev/Solodev.zip
 cd /tmp/Solodev/
