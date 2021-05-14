2d4f7514ba27b796aa10d5a093a0ffb388146e44
Ross Williams <ross@solodev.com>
2021-05-14 10:55:11 -0400

    version locking clean-css

--- hashicorp/solodev-phpfpm-72/aws2/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/aws2/scripts/install-node.sh
@@ -3,7 +3,7 @@ yum install -y --enablerepo=nodesource nodejs
 
 mkdir -p "/var/www/node_modules_global"
 npm config set prefix "/var/www/node_modules_global"
-npm install -g --unsafe-perm @fortawesome/fontawesome-free autoprefixer clean-css-cli node-sass npm-run-all postcss postcss-cli gulp gulp-autoprefixer \
+npm install -g --unsafe-perm @fortawesome/fontawesome-free autoprefixer clean-css-cli@4.3.0 node-sass npm-run-all postcss postcss-cli gulp gulp-autoprefixer \
 		gulp-clean gulp-clean-css gulp-concat gulp-rename gulp-sass gulp-uglify node-sass
 
 mkdir /var/www/.npm
--- hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
+++ hashicorp/solodev-phpfpm-72/single/scripts/install-node.sh
@@ -1,7 +1,7 @@
 curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
 yum install -y --enablerepo=nodesource nodejs
 
-npm install -g --unsafe-perm @fortawesome/fontawesome-free autoprefixer clean-css-cli node-sass npm-run-all postcss postcss-cli gulp gulp-autoprefixer \
+npm install -g --unsafe-perm @fortawesome/fontawesome-free autoprefixer clean-css-cli@4.3.0 node-sass npm-run-all postcss postcss-cli gulp gulp-autoprefixer \
 		gulp-clean gulp-clean-css gulp-concat gulp-rename gulp-sass gulp-uglify node-sass
 
 mkdir /var/www/.npm
