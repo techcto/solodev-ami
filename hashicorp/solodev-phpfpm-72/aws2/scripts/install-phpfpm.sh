0d3e70c14f41855ce622f79842e719a852a2d0f9
techcto <smoore@solodev.com>
2021-06-21 19:50:16 -0400

    ..

--- hashicorp/solodev-phpfpm-72/aws2/scripts/install-phpfpm.sh
+++ hashicorp/solodev-phpfpm-72/aws2/scripts/install-phpfpm.sh
@@ -13,15 +13,15 @@ yum -y install tidy
 #Install PHP-FPM 7.2
 yum --enablerepo=remi-php72 install -y php72-php-fpm php72-php-common \
 php72-php-devel php72-php-mysqli php72-php-mysqlnd php72-php-pdo_mysql \
-php72-php-gd php72-php-mbstring php72-php-pear php72-php-soap php72-php-zip php72-php-tidy \
-php72-php-pecl-mongodb php72-php-pecl-apcu php72-php-pecl-oauth --skip-broken
+php72-php-gd php72-php-mbstring php72-php-pear php72-php-soap php72-php-tidy \
+php72-php-pecl-mongodb php72-php-pecl-apcu php72-php-pecl-oauth
 scl enable php72 'php -v'
 ln -s /usr/bin/php72 /usr/bin/php
 
 #Tmp hack
 #note: remove --skip-broken
-rpm -e --nodeps libzip5
-yum install -y libzip
+# rpm -e --nodeps libzip5
+yum --enablerepo=remi-php72 install -y libzip php72-php-zip
 
 #Configure PHP-FPM conf for Apache (php72-php.conf)
 rm -Rf /etc/httpd/conf.d/php.conf
